211a3ad716b374360bb7314edaeba3f5c3ee374d
Scravlon <haokokyong10@gmail.com>
2019-09-23 10:04:08 -0400

    Merge remote-tracking branch 'remotes/origin/login' into Develop

--- app/src/main/java/com/cse442/olmcdonald/MainActivity.java
+++ app/src/main/java/com/cse442/olmcdonald/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+
 public class MainActivity extends AppCompatActivity {
     LinearLayout peachview, blueberryview,tomatoview,spinachview,appleview,cherryview,figview,grapeview,orangeview;
     @Override
