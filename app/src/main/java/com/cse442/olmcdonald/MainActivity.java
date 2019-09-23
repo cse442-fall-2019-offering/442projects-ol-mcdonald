16530a2844baef7507b9c27ffdda6d070ccafad3
Scravlon <haokokyong10@gmail.com>
2019-09-23 10:05:30 -0400

    MainActivity updated

--- app/src/main/java/com/cse442/olmcdonald/MainActivity.java
+++ app/src/main/java/com/cse442/olmcdonald/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
-
 public class MainActivity extends AppCompatActivity {
     LinearLayout peachview, blueberryview,tomatoview,spinachview,appleview,cherryview,figview,grapeview,orangeview;
     @Override
